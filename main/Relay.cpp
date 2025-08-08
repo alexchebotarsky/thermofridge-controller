fd13ed13fb9b16b96879290376391b28998f45ff
alexchebotarsky <alexchebotarsky@gmail.com>
2025-08-08 10:03:15 +0200

    Fix relays

--- main/Relay.cpp
+++ main/Relay.cpp
@@ -3,11 +3,17 @@
 Relay::Relay(const int gpio_pin) : gpio(static_cast<gpio_num_t>(gpio_pin)) {}
 
 esp_err_t Relay::init() {
+  // Off by default
+  esp_err_t err = gpio_set_level(gpio, 1);
+  if (err != ESP_OK) {
+    return err;
+  }
+
   gpio_config_t config = {};
   config.mode = GPIO_MODE_OUTPUT;
   config.pin_bit_mask = 1ULL << gpio;
 
-  esp_err_t err = gpio_config(&config);
+  err = gpio_config(&config);
   if (err != ESP_OK) {
     return err;
   }
@@ -16,7 +22,7 @@ esp_err_t Relay::init() {
 }
 
 esp_err_t Relay::on() {
-  esp_err_t err = gpio_set_level(gpio, 1);
+  esp_err_t err = gpio_set_level(gpio, 0);
   if (err != ESP_OK) {
     return err;
   }
@@ -24,7 +30,7 @@ esp_err_t Relay::on() {
   return ESP_OK;
 }
 esp_err_t Relay::off() {
-  esp_err_t err = gpio_set_level(gpio, 0);
+  esp_err_t err = gpio_set_level(gpio, 1);
   if (err != ESP_OK) {
     return err;
   }
